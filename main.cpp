909e8882d30f45283f8d06b6052093c1a11093eb
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-09 11:42:54 -0400

    Fixing some bugs with main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include "console/EstablecerConsola.hpp"
+#include "utils/FuncionesAuxiliares.hpp"
+
+
+#define ENTER 13
+#define ESCAPE 27
 
 using namespace std;
 
 int main()
 {
+
+    char tecla;
+    int m, n;
+    cout << "Capturando las dimensiones del tablero... \n\n";
+    cout << "MAX (" << MAXREN << ") ";
+    do {
+        m = CapturarEntero("Filas: ");
+        if (m > MAXREN) {
+            cout << "El numero NO puede ser mayor a " << MAXREN << ". ";
+        }
+    } while (m < 1 || m > MAXREN);
+    cout << endl;
+    cout << "MAX (" << MAXCOL << ") ";
+    do {
+        n = CapturarEntero("Columnas: ");
+        if (n > MAXCOL) {
+            cout << "El numero NO puede ser mayor a " << MAXCOL << ". ";
+        }
+    } while (n < 1 || n > MAXCOL);
+    CambiarCursor(APAGADO, NORMAL);
     return 0;
 }
+
+
